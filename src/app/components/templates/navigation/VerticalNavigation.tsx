f783f0ea9c2bd14bea2906162af195f330267712
Alexander Janssen <ajn.janssen@gmail.com>
2024-12-22 02:25:46 +0100

    Some styling

--- src/app/components/templates/navigation/VerticalNavigation.tsx
+++ src/app/components/templates/navigation/VerticalNavigation.tsx
@@ -4,15 +4,24 @@ import React from "react";
 import NavigationListItems from "@/app/components/organisms/navigation/list/navigationListItems";
 import { navigationItems } from "@/app/config/navigation/navigationConfig";
 import FlexBox from "@/app/components/templates/flex/FlexBox";
-import InnerListItem from "@/app/components/molecules/navigation/vertical/innerListItem";
+import Container from "@/app/components/templates/container/container";
 
 const VerticalNavigation = () => {
   return (
-    <FlexBox flexDirection="Column" padding="P6" height="HScreen">
-      <FlexBox flexDirection="Column" padding="P6" height="HFull" gap="Gap4">
-        <NavigationListItems items={navigationItems} />
-      </FlexBox>
-    </FlexBox>
+    <Container>
+      <div className="glass isolate aspect-video w-96 rounded-xl shadow-lg ring-1 ring-black/5 backdrop-filter backdrop-blur-lg">
+        <FlexBox flexDirection="Column" padding="P6" height="HScreen">
+          <FlexBox
+            flexDirection="Column"
+            padding="P6"
+            height="HFull"
+            gap="Gap4"
+          >
+            <NavigationListItems items={navigationItems} />
+          </FlexBox>
+        </FlexBox>
+      </div>
+    </Container>
   );
 };
 
--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -3,6 +3,7 @@ import InnerLayout from "@/app/components/templates/layout/InnerLayout";
 import VerticalNavigation from "@/app/components/templates/navigation/VerticalNavigation";
 import GridBox from "@/app/components/templates/grid/GridBox";
 import GridInner from "@/app/components/templates/grid/GridInner";
+import Container from "../components/templates/container/container";
 
 export default function RootLayout({
   children,
@@ -13,14 +14,17 @@ export default function RootLayout({
     <InnerLayout>
       <GridBox
         gridTemplateColumns="Col4"
-        gap="Gap4"
+        gap="Gap8"
         justifyItems="Start"
         gridAutoFlow="ColDense"
       >
         <GridInner colSpan="ColSpan1">
           <VerticalNavigation />
         </GridInner>
-        <GridInner colSpan="ColSpan3">{children}</GridInner>
+
+        <GridInner colSpan="ColSpan3">
+          <Container>{children}</Container>
+        </GridInner>
       </GridBox>
     </InnerLayout>
   );
