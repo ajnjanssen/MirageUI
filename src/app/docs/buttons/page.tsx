918bb38177f75d4a6a25f5ea853b1989fe776c44
Alexander Janssen <ajn.janssen@gmail.com>
2024-12-25 12:50:41 +0100

    Update page.tsx

--- src/app/docs/buttons/page.tsx
+++ src/app/docs/buttons/page.tsx
@@ -7,18 +7,8 @@ function page() {
   return (
     <>
       <Container>
-        <Heading level={1} text="Buttons" color="text-primary" />
-        <Container grid cols="grid-cols-3" gap="gap-4">
-          <Button text="Primary Button" type="primary" />
-          <Button text="Secondary Button" type="secondary" />
-          <Button text="Base-100 Button" type="base-100" />
-          <Button text="Base-200 Button" type="base-200" />
-          <Button text="Base-300 Button" type="base-300" />
-          <Button text="Base Content Button" type="base-content" />
-          <Button text="Warning Button" type="warning" />
-          <Button text="Error Button" type="error" />
-          <Button text="Disabled Button" type="disabled" disabled={true} />
-        </Container>
+        <Heading level={1} text="Buttons" color="primary" />
+        <Container grid cols="grid-cols-3" gap="gap-4"></Container>
       </Container>
     </>
   );
